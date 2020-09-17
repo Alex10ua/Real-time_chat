6885f6ff971a746418d1d7ba0a5193e98bd349b9
alex <sasha.lazoriak@gmail.com>
2020-09-17 13:05:14 +0200

    [update] Updated WebSocket config

--- src/main/java/com/dev/test_task/config/Config.java
+++ src/main/java/com/dev/test_task/config/Config.java
@@ -5,17 +5,17 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class Config implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/dev").withSockJS();
-    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();;
     }
 }
